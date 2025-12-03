552a15b806b51f6afde8165d59da15f0a25e9465
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-03 19:26:58 +0530

    Understood the concepts of Strings to number conversion

--- Conversion.js
+++ Conversion.js
@@ -5,6 +5,11 @@
 
 // console.log(valueInN);
 
-let present=1;
-let isTheStudentPresent= Boolean (present);
-console.log(isTheStudentPresent);
+// let present=1;
+// let isTheStudentPresent= Boolean (present);
+// console.log(isTheStudentPresent);
+
+console.log("1"+2+3+4);
+console.log(1+2+3+4+"5");  // in js converison, if first character is a string, the entire number will convert to a string on priority else an integer value
+console.log(1+"2"+3+4+"5");  // in js converion of this case, we can see multiple string value, so to maintain the logic , entire thing is converted to string.
+
