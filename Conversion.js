dbf3950b5ec35248e5b38df77e49d6ddb8a191ab
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-03 16:58:23 +0530

    Understood the concepts of basic conversion

---
+++ Conversion.js
@@ -0,0 +1,10 @@
+// let yu= "69";
+
+// let valueInN= Number (yu);
+// console.log(yu);
+
+// console.log(valueInN);
+
+let present=1;
+let isTheStudentPresent= Boolean (present);
+console.log(isTheStudentPresent);
