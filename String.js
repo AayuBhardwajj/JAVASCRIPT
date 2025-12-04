79022d263b0380406ade9d29dd4836ee21fdadda
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-04 20:49:06 +0530

    Understood the concepts of Strings in js, along with its functions

---
+++ String.js
@@ -0,0 +1,18 @@
+name="Aayush";
+age=20;
+console.log(`My name is ${name} and my age is ${age}`);  // string interpolation
+
+
+let naam= "  Aayush ";
+console.log(naam.length);
+console.log(naam.indexOf('y'));
+console.log(naam.charAt(2));
+console.log(naam.substring(0,5));
+console.log(naam.slice(0,5));
+console.log(naam.slice(-1,5));
+console.log(naam.trim());
+console.log(naam.replace("A" , "X"));
+console.log(naam.includes("y"));
+
+let naam1="Aa-yu-sh";
+console.log(naam1.split("-"));
