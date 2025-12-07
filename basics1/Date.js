3a955ba79cbacc94f39cbcc27cf094a1077a443f
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-07 12:58:52 +0530

    Updated the folder structure and understood some concepts about date and time.

---
+++ basics1/Date.js
@@ -0,0 +1,18 @@
+// let newDate= new Date();
+// console.log(newDate);
+// console.log(newDate.toString())
+// console.log(newDate.toDateString())
+// console.log(newDate.toISOString());
+// console.log(newDate.toJSON());
+// console.timeLog(newDate.toLocaleString());
+
+//  let myDate= new Date(2023, 2, 13, 10, 43,57);
+//  console.log(myDate.toLocaleString());
+//  console.log(myDate.toLocaleTimeString())
+
+ let newDate= new Date("01-4-2025");
+// console.log(newDate.toLocaleDateString());
+
+console.log(Math.floor(Date.now()/1000));
+console.log(newDate.getMonth());
+
