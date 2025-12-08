b20ccceeb883c56b2c12bbc22ccac83c258bc4eb
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-08 11:04:30 +0530

    Understood about the concepts of array

--- basics2/arrays.js
+++ basics2/arrays.js
@@ -18,17 +18,42 @@ let myArray=[1,2,3,4,5,6];
 
 // console.log(typeof newArray);
 
-let arr1= myArray.slice(0,3);
-console.log();
-console.log(`Array after slicing: ${myArray}`);
-console.log();
+// let arr1= myArray.slice(0,3); 
+// console.log();
+// console.log(`Array after slicing: ${myArray}`);
+// console.log();
 
-console.log(`Sliced part: ${arr1}`);
+// console.log(`Sliced part: ${arr1}`);
 
-let arr2= myArray.splice(0,2);
-console.log();
-console.log(`Array after splicing: ${myArray}`);
-console.log();
+// let arr2= myArray.splice(0,2);
+// console.log();
+// console.log(`Array after splicing: ${myArray}`);
+// console.log();
 
-console.log(`Slpiced part: ${arr2}`);
+// console.log(`Slpiced part: ${arr2}`);
 
+let arr1=["Ironman","Batman","SuperMan"];
+let arr2=["Joker","Thor","Loki"];
+// arr1.push(arr2)
+// console.log(`If I use push operation, the output would be: ${arr1}`); 
+// let mergedArray= arr1.concat(arr2);
+
+// console.log(`If i use concat operation, the output would be ${mergedArray}`);
+
+const all_heroes=[...arr1, ...arr2] // The concept of spreading, the other name for concatination
+console.log(all_heroes);
+
+let fArray=[1,2,3,["Hello","There","Hey"],5,6,["True","False",["Aayush"]]];
+let clearArray=fArray.flat(); // .flat deals in simplifying the array inside array thing.
+console.log(clearArray);
+
+console.log(Array.isArray("Aayush")); // Array.isArray( " ") checks whether the argument we pass is an array or not.
+console.log(Array.from("Aayush")); // Array .from(" ") creates the arguement passed in array. it is used when we need to take data from websites thst might be in any pther data structure and convert it into array.
+
+let name1="SQl";
+let name2="MySQL";
+let name3="PostSQL";
+
+console.log(Array.of(name1,name2,name3)); // Array.of function creates and returns a new array.
+
+//Array.is, from, of 
