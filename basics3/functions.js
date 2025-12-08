b61a728c1342ddaa955c62818f77f4c27a255a9f
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-08 14:30:35 +0530

    Understood the basics of functions in Js

---
+++ basics3/functions.js
@@ -0,0 +1,17 @@
+// function myName(){
+//     console.log("My name is pineapple");
+// }
+
+// myName();
+
+function addition(a, b){
+    console.log(a+b);
+}
+
+addition(5,7);
+
+function loggedIn(user){
+    return `${user} just logged in`;
+}
+
+console.log(loggedIn("Aayush"));
