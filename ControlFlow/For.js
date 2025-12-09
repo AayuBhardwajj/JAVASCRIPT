8fc42093eb6eae9592ff9a1420ade2d3becf7311
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-09 11:06:30 +0530

    Understood the concepts of for loop, break and continue statements.

---
+++ ControlFlow/For.js
@@ -0,0 +1,13 @@
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+    
+// }
+
+for(let i=0;i<3;i++){
+    console.log(`Outer loop is having the value of: ${i}`);
+    for(let j=0;j<3;j++){
+        console.log(`Value of inner loop: ${j} && Value of outer loop: ${i}`)
+    }
+}
+
+// break and continue keyword.
