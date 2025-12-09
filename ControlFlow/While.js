d13b7224339f361ce2ddf42a9f37c1c284905593
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-09 11:15:46 +0530

    Understood the concepts of while loop in JS

---
+++ ControlFlow/While.js
@@ -0,0 +1,13 @@
+// let i=0;
+// while(i<3){
+//     console.log(i);
+//     i++;
+// }
+
+let myAr=["Tony Stark","Michel Stark","Doraemon"];
+let i=0;
+while(i<myAr.length){
+    let ele=myAr[i];
+    console.log(ele);
+    i++;
+}
