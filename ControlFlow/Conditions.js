d07f3038025c769755bdd344da8f20a3dad2ae40
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-09 10:45:52 +0530

    Understood the concepts of if-else conditions, switch statements in JS

---
+++ ControlFlow/Conditions.js
@@ -0,0 +1,13 @@
+if(2=="2"){
+    console.log("Yes, 2 is equal to '2' ");
+}
+
+// Major difference between == and === is , triple = also checks the data type. 
+
+if(2==="2"){
+    console.log("Yes, 2 is equal to '2' ");
+}else{
+    console.log("No, 2 is not equal to '2'")
+}
+
+ // Also includes switch cases.
