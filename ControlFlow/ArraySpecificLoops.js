3a4a786763380b5941212d555fad40d1c8f33923
AayuBhardwajj <bhardwajaayush26@gmail.com>
2025-12-09 15:26:09 +0530

    Understood about the concepts of map function

--- ControlFlow/ArraySpecificLoops.js
+++ ControlFlow/ArraySpecificLoops.js
@@ -1,84 +1,88 @@
-// let myAr=["Harry","Sharry","Garry","Aerry"];
+// // let myAr=["Harry","Sharry","Garry","Aerry"];
 
-// for (const i of myAr) {
-//     console.log(myAr);
+// // for (const i of myAr) {
+// //     console.log(myAr);
     
+// // }
+
+// // let hey="Hello Ji!!";
+// // for (const hi of hey) {
+// //     console.log(`${hi}`);
+    
+// // }
+
+// let m= new Map();  // Holds data in key-value pairs. doesnt allows duplicated value and maintians the original order throughout
+ 
+// m.set(1,"India");
+// m.set(2,"Australia");
+// m.set(3,"Russia");
+// m.set(1,"India");
+
+// for(let [key, value] of m){
+//     console.log(key, ":" , value);        // Tareeka of printing key-value pairs of map. 
 // }
 
-// let hey="Hello Ji!!";
-// for (const hi of hey) {
-//     console.log(`${hi}`);
-    
+// // for loop in case of traversing object
+
+// let myObj={
+//     name: "Aayush",
+//     age:20,
+//     branch:"CSE"
 // }
 
-let m= new Map();  // Holds data in key-value pairs. doesnt allows duplicated value and maintians the original order throughout
+// for(let r in myObj){
+//     console.log(`${r} : ${myObj[r]}`)  // Used for in loop to traverse object, printing "r" would only print keys and printing myObj[r] would print values also.
+    
+// }
  
-m.set(1,"India");
-m.set(2,"Australia");
-m.set(3,"Russia");
-m.set(1,"India");
+// // for of loop prints the value directly while for in loop prints the keys.
 
-for(let [key, value] of m){
-    console.log(key, ":" , value);        // Tareeka of printing key-value pairs of map. 
-}
 
-// for loop in case of traversing object
+// // for each loop
+// console.log(".........................");
 
-let myObj={
-    name: "Aayush",
-    age:20,
-    branch:"CSE"
-}
+// let coding=["Java","JavaScript","Python"];
+// coding.forEach(function(i){
+//     console.log(i);
+// })
+// console.log(".........................");
+// coding.forEach((i)=>{
+//     console.log(i);
+// })
+// console.log(".........................");
+// function printMe(x){
+//     console.log(x);
+// }
 
-for(let r in myObj){
-    console.log(`${r} : ${myObj[r]}`)  // Used for in loop to traverse object, printing "r" would only print keys and printing myObj[r] would print values also.
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, arr)=>{
+//     console.log(item,index,arr);
     
-}
- 
-// for of loop prints the value directly while for in loop prints the keys.
+// })
+// console.log(".........................");
+// let codingg=[
+//     {
+//     language: "JavaScript",
+//     fileName: "Js"
+//     },
+//     {
+//         language: "Python",
+//         fileName: "Py"
+//     },
+//     {
+//         language:"Java",
+//         fileName:"Java"
+//     }
+// ]
 
-
-// for each loop
-console.log(".........................");
-
-let coding=["Java","JavaScript","Python"];
-coding.forEach(function(i){
-    console.log(i);
-})
-console.log(".........................");
-coding.forEach((i)=>{
-    console.log(i);
-})
-console.log(".........................");
-function printMe(x){
-    console.log(x);
-}
-
-coding.forEach(printMe);
-
-coding.forEach((item, index, arr)=>{
-    console.log(item,index,arr);
+// codingg.forEach((item)=>{
+//     // console.log(item.fileName);
+//     // console.log(item.language);
+//     console.log(`${item.language} : ${item.fileName}`);
     
-})
-console.log(".........................");
-let codingg=[
-    {
-    language: "JavaScript",
-    fileName: "Js"
-    },
-    {
-        language: "Python",
-        fileName: "Py"
-    },
-    {
-        language:"Java",
-        fileName:"Java"
-    }
-]
+// })
 
-codingg.forEach((item)=>{
-    // console.log(item.fileName);
-    // console.log(item.language);
-    console.log(`${item.language} : ${item.fileName}`);
-    
-})
+nums=[1,2,3,4,5,6,7,8];
+let answer=nums.filter((num)=> num>2);
+console.log(answer);
