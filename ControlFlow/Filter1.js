eb2eee6ff09ddd1dc11b6be5e1da45d779139fbd
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-01-24 23:36:18 +0530

    Recalled about chaining

--- ControlFlow/Filter1.js
+++ ControlFlow/Filter1.js
@@ -1,3 +1,6 @@
 const myNums=[1,2,3,4,5,6,7,8,9,10]
-const fil= myNums.filter((num)=> num>3);
-console.log(fil);
+// const fil= myNums.filter((num)=> num>3);
+// console.log(fil);
+
+const value=myNums.map( (i) => i*10).filter((i)=>i>20);
+console.log(value);
