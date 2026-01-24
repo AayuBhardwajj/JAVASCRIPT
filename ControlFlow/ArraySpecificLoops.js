c8b94ebb7d409c260c55416167e61dbf90f46f65
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-01-24 22:48:19 +0530

    Reunderstood about arrow functions

--- ControlFlow/ArraySpecificLoops.js
+++ ControlFlow/ArraySpecificLoops.js
@@ -1,22 +1,22 @@
-// // let myAr=["Harry","Sharry","Garry","Aerry"];
+let myAr=["Harry","Sharry","Garry","Aerry"];
 
-// // for (const i of myAr) {
-// //     console.log(myAr);
+for (const i of myAr) {
+    console.log(myAr);
     
-// // }
+}
 
-// // let hey="Hello Ji!!";
-// // for (const hi of hey) {
-// //     console.log(`${hi}`);
+let hey="Hello Ji!!";
+for (const hi of hey) {
+    console.log(`${hi}`);
     
-// // }
+}
 
-// let m= new Map();  // Holds data in key-value pairs. doesnt allows duplicated value and maintians the original order throughout
+let m= new Map();  // Holds data in key-value pairs. doesnt allows duplicated value and maintians the original order throughout
  
-// m.set(1,"India");
-// m.set(2,"Australia");
-// m.set(3,"Russia");
-// m.set(1,"India");
+m.set(1,"India");
+m.set(2,"Australia");
+m.set(3,"Russia");
+m.set(1,"India");
 
 // for(let [key, value] of m){
 //     console.log(key, ":" , value);        // Tareeka of printing key-value pairs of map. 
@@ -24,62 +24,62 @@
 
 // // for loop in case of traversing object
 
-// let myObj={
-//     name: "Aayush",
-//     age:20,
-//     branch:"CSE"
-// }
+let myObj={
+    name: "Aayush",
+    age:20,
+    branch:"CSE"
+}
 
-// for(let r in myObj){
-//     console.log(`${r} : ${myObj[r]}`)  // Used for in loop to traverse object, printing "r" would only print keys and printing myObj[r] would print values also.
+for(let r in myObj){
+    console.log(`${r} : ${myObj[r]}`)  // Used for in loop to traverse object, printing "r" would only print keys and printing myObj[r] would print values also.
     
-// }
+}
  
 // // for of loop prints the value directly while for in loop prints the keys.
 
 
 // // for each loop
-// console.log(".........................");
+console.log(".........................");
 
-// let coding=["Java","JavaScript","Python"];
-// coding.forEach(function(i){
-//     console.log(i);
-// })
-// console.log(".........................");
-// coding.forEach((i)=>{
-//     console.log(i);
-// })
-// console.log(".........................");
-// function printMe(x){
-//     console.log(x);
-// }
+let coding=["Java","JavaScript","Python"];
+coding.forEach(function(i){
+    console.log(i);
+})
+console.log(".........................");
+coding.forEach((i)=>{
+    console.log(i);
+})
+console.log(".........................");
+function printMe(x){
+    console.log(x);
+}
 
-// coding.forEach(printMe);
+coding.forEach(printMe);
 
-// coding.forEach((item, index, arr)=>{
-//     console.log(item,index,arr);
+coding.forEach((item, index, arr)=>{
+    console.log(item,index,arr);
     
-// })
-// console.log(".........................");
-// let codingg=[
-//     {
-//     language: "JavaScript",
-//     fileName: "Js"
-//     },
-//     {
-//         language: "Python",
-//         fileName: "Py"
-//     },
-//     {
-//         language:"Java",
-//         fileName:"Java"
-//     }
-// ]
+})
+console.log(".........................");
+let codingg=[
+    {
+    language: "JavaScript",
+    fileName: "Js"
+    },
+    {
+        language: "Python",
+        fileName: "Py"
+    },
+    {
+        language:"Java",
+        fileName:"Java"
+    }
+]
 
-// codingg.forEach((item)=>{
-//     // console.log(item.fileName);
-//     // console.log(item.language);
-//     console.log(`${item.language} : ${item.fileName}`);
+codingg.forEach((item)=>{
+    // console.log(item.fileName);
+    // console.log(item.language);
+    console.log(`${item.language} : ${item.fileName}`);
     
-// })
+})
 
--- ControlFlow/Filter.js
+++ ControlFlow/Filter.js
@@ -1,48 +1,48 @@
-// const books = [
-//   {
-//     title: "The Alchemist",
-//     genre: "Fiction",
-//     publish: 1988,
-//     edition: 2014
-//   },
-//   {
-//     title: "Atomic Habits",
-//     genre: "Self-Help",
-//     publish: 2018,
-//     edition: 2022
-//   },
-//   {
-//     title: "Clean Code",
-//     genre: "Programming",
-//     publish: 2008,
-//     edition: 2021
-//   },
-//   {
-//     title: "The Psychology of Money",
-//     genre: "Finance",
-//     publish: 2020,
-//     edition: 2023
-//   },
-//   {
-//     title: "Harry Potter and the Sorcerer's Stone",
-//     genre: "Fantasy",
-//     publish: 1997,
-//     edition: 2019
-//   }
-// ];
+const books = [
+  {
+    title: "The Alchemist",
+    genre: "Fiction",
+    publish: 1988,
+    edition: 2014
+  },
+  {
+    title: "Atomic Habits",
+    genre: "Self-Help",
+    publish: 2018,
+    edition: 2022
+  },
+  {
+    title: "Clean Code",
+    genre: "Programming",
+    publish: 2008,
+    edition: 2021
+  },
+  {
+    title: "The Psychology of Money",
+    genre: "Finance",
+    publish: 2020,
+    edition: 2023
+  },
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    genre: "Fantasy",
+    publish: 1997,
+    edition: 2019
+  }
+];
 
-// // let xx=books.filter((bk)=> bk.genre==="Fiction");
-// let xx=books.filter((bk)=>bk.publish>2000);
-// console.log(xx);
+// let xx=books.filter((bk)=> bk.genre==="Fiction");
+let xx=books.filter((bk)=>bk.publish>2000);
+console.log(xx);
 
 // nums=[1,2,3,4,5,6,7,8];
 // let answer=nums.filter((num)=> num>2); // filter returns a value, for each doesn't.
 // console.log(answer);
 
-let nums=[1,2,3,4,5,6,7,8,9,10];
-let ans= nums.map((x)=>x*10).map((x)=> x+1).filter((x)=> x>80);
-console.log(ans);  
+// let nums=[1,2,3,4,5,6,7,8,9,10];
+// let ans= nums.map((x)=>x*10).map((x)=> x+1).filter((x)=> x>80);
+// console.log(ans);  
 
-// Basic difference between filter and map, filter accepts value on the basis of true or false. If condition is true, filter allows the further execution
-// in case of map, map focuses more on modifying the data.
+// // Basic difference between filter and map, filter accepts value on the basis of true or false. If condition is true, filter allows the further execution
+// // in case of map, map focuses more on modifying the data.
 
