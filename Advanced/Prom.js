f64ddf9bb3894283f3d9a1a1908e1034ede3fb6e
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-01-27 23:17:19 +0530

    Understood a bit about promises

---
+++ Advanced/Prom.js
@@ -0,0 +1,15 @@
+//--The Promise object represents the eventual completion (or failure) of an
+// asynchronous operation and its resulting value.-->
+
+//--Jo bhi task diya hai, vo abhi ke abhi queue me lag ke complete nhi hoga. Queue me lg gya hai, abhi ke abhi usko load krke complete nhi kr skte  -->
+const prom=new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task is completed");   
+        resolve();  
+    }, 1000)
+});
+
+prom.then(function(){
+    console.log("Promise consumed");
+    
+})  ///.then() is related with resolve.. it returns the value recieved in promise
