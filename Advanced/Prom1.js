16949d91a8f8b80b67f51d57b16f6ef0114df35d
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-02-01 16:09:43 +0530

    Understood how multiple values are passes in resolve()

--- Advanced/Prom1.js
+++ Advanced/Prom1.js
@@ -1,22 +1,48 @@
-const prom2= new Promise((resolve,reject)=>{
+// const prom2= new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         let err=true;
+//         if(err){
+//             resolve({userName: "Aayush",
+//                         password: "123#"
+//             });
+//         }else{
+//             reject("Error occured");
+//         }
+//     },1000)
+// })
+
+// prom2.then((ab)=>{
+//     console.log(ab);
+//     return ab.password;
+// }).then((vq)=>{
+//     console.log(`The password is: ${vq}`);
+// }).catch((err)=>{
+//     console.log("promise rejected",err);
+// })
+
+
+new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        let err=true;
-        if(err){
-            resolve({userName: "Aayush",
-                        password: "123#"
+        const decision=true;
+        if(decision){
+            resolve({UserName: "Aayush",
+                    password: "123$"
             });
         }else{
             reject("Error occured");
         }
     },1000)
-})
-
-prom2.then((ab)=>{
-    console.log(ab);
-    return ab.password;
-}).then((vq)=>{
-    console.log(`The password is: ${vq}`);
-}).catch((err)=>{
-    console.log("promise rejected",err);
+}).then((ab)=>{
+    const data=({
+        name:ab.UserName,
+        pass:ab.password
+    });
+    return data;
+}).then((xy)=>{
+    console.log(`User name is: ${xy.name}`);
+    console.log(`Password is: ${xy.pass}`);
+}).catch((w)=>{
+    console.log(w);
     
 })
+
