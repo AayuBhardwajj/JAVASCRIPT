5d915f87d6dd729014405ed96fe9f1fad3141ac9
AayuBhardwajj <bhardwajaayush26@gmail.com>
2026-02-27 16:43:56 +0530

    Brushed about the concepts of higher order functions, Reduce

--- Advanced/HOF/1map.js
+++ Advanced/HOF/1map.js
@@ -2,4 +2,5 @@ const arr=[1,2,3,4,5]
 
 const fn=arr.map((x)=> x*x);
 console.log(fn);
+console.log(arr.map((x)=>x+x));
 
---
+++ Advanced/HOF/1redu.js
@@ -0,0 +1,4 @@
+const arr=[1,2,3,4];
+
+let sum= arr.reduce((a,b)=>a+b,0);
+console.log(sum);
